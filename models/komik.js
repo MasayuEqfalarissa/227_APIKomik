05dbf2e4862d516f2cfcebc710f57cf32d458b48
MasayuEqfalarissa <masayueqfalarissaa@gmail.com>
2026-08-01 22:00:05 +0700

    feat: menambahkan atribut author pada model komik

--- models/komik.js
+++ models/komik.js
@@ -11,6 +11,9 @@ module.exports = (sequelize, DataTypes) => {
         description: {
             type: DataTypes.STRING
         },
+         author: {
+            type: DataTypes.STRING
+        } 
         
     });
     return Komik;
